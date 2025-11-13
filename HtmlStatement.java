d8fafa6d269c64c67df8085a6c7efb9d077585cb
eduardofernandez197 <eduardofefe1970@gmail.com>
2025-11-13 19:51:19 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -2,7 +2,8 @@ public class HtmlStatement extends Statement {
 
     @Override
     protected String headerString(Customer aCustomer) {
-        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+        return "<H1>Rentals for <EM>" + aCustomer.getName() +
+               "</EM></H1><P>\n";
     }
 
     @Override
@@ -13,7 +14,8 @@ public class HtmlStatement extends Statement {
 
     @Override
     protected String footerString(Customer aCustomer) {
-        return "<P>You owe <EM>" + aCustomer.getTotalCharge() + "</EM><P>\n" +
+        return "<P>You owe <EM>" + aCustomer.getTotalCharge() +
+               "</EM><P>\n" +
                "On this rental you earned <EM>" +
                aCustomer.getTotalFrequentRenterPoints() +
                "</EM> frequent renter points<P>";
